ec27eafce0546941b1b1b56c67bb36ebe5dc26f8
Lahiru Mahagamage <lahiru.nc@gmail.com>
2022-01-02 18:47:03 +1100

    final

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "getxamplify": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
